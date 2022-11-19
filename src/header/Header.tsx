4b4819910349eb3364838048ac72e8f662a0ea6f
Mateoox600 <matboi862@gmail.com>
2022-11-19 15:27:22 +0100

    Updating burger menu

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { MouseEvent, useState } from 'react';
 import menu from './menu.svg';
 import './Header.css';
 import github from '../icons/github.png';
@@ -7,11 +7,18 @@ import linkedIn from '../icons/linkedIn.png';
 export default function Header() {
 
     const [ scrolling, setScrolling ] = useState(false);
+    const [ burgerOpen, setBurgerOpen ] = useState(false);
 
     window.addEventListener('scroll', () => {
         if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) setScrolling(true);
         else setScrolling(false);
     });
+
+    function checkWidthOnClick(e: MouseEvent) {
+        if(e.pageX < window.innerWidth / 2) {
+            setBurgerOpen(false);
+        }
+    }
 
     return (
         <div className={`header ${scrolling ? 'scrolling' : ''}`}>
@@ -26,12 +33,15 @@ export default function Header() {
                 </div>
             </div>
             <div className='burger-nav'>
-                <img className='burger-menu' src={menu} alt="" />
-                <div className='menu'>
-                    <a href='#presentation' className='burger-item'>Presentation</a>
-                    <a href="#techno" className='burger-item'>Knowledge</a>
-                    <a href='#projects' className='burger-item'>Projects</a>
-                    <a href='#contact' className='burger-item'>Contact</a>
+                <img onClick={() => setBurgerOpen(!burgerOpen)} className={`burger-menu ${burgerOpen ? 'open' : ''}`} src={menu} alt="" />
+                <div className={`menu ${burgerOpen ? 'open' : ''}`} onClick={checkWidthOnClick}>
+                    <a onClick={() => setBurgerOpen(false)} href='#presentation' className={`burger-item ${burgerOpen ? '' : 'disabled'}`}>Presentation</a>
+                    <a onClick={() => setBurgerOpen(false)} href="#techno" className={`burger-item ${burgerOpen ? '' : 'disabled'}`}>Knowledge</a>
+                    <a onClick={() => setBurgerOpen(false)} href='#projects' className={`burger-item ${burgerOpen ? '' : 'disabled'}`}>Projects</a>
+                    <div onClick={() => setBurgerOpen(false)} className={`burger-item ${burgerOpen ? '' : 'disabled'}`} id='burger-contacts'>
+                        <a className='burger-contacts-link' href="https://github.com/mateoox600" target='_blank' rel='noreferrer'><img src={github} alt="" /></a>
+                        <a className='burger-contacts-link' href="https://www.linkedin.com/in/mathys-g" target='_blank' rel='noreferrer'><img src={linkedIn} alt="" /></a>
+                    </div>
                 </div>
             </div>
         </div>
